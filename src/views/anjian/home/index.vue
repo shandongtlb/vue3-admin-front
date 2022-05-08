aa2dfa7e04031e21f6e327aa7d6e631e9b170377
SDTLB <40312509+shandongtlb@users.noreply.github.com>
2022-05-08 19:10:28 +0800

    规范化

--- src/views/anjian/home/index.vue
+++ src/views/anjian/home/index.vue
@@ -4,17 +4,19 @@
 
 <script>
   module.exports = {
-    created () {
+    created() {
       this.chartData = {
         dimensions: {
           name: 'Week',
-          data: ['Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fir.', 'Sat.', 'Sun.']
+          data: ['Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fir.', 'Sat.', 'Sun.'],
         },
-        measures: [{
-          name: 'Vue',
-          data: [30, 40, 35, 50, 49, 70, 90]
-        }]
-      }
-    }
-  }
+        measures: [
+          {
+            name: 'Vue',
+            data: [30, 40, 35, 50, 49, 70, 90],
+          },
+        ],
+      };
+    },
+  };
 </script>
