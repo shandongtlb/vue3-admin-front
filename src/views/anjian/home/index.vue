c459b2abd5aa277f6a66a58a6952430d9082ec05
shandongtlb <wangyang19980320@hotmail.com40312509+shandongtlb@users.noreply.github.com>
2022-05-12 00:37:47 +0800

    add echart5

--- src/views/anjian/home/index.vue
+++ src/views/anjian/home/index.vue
@@ -1,22 +1,38 @@
 <template>
-  <ve-line-chart :data="chartData" />
+  <div>
+    <div id="leibie" class="leibie-pie"></div>
+  </div>
 </template>
 
 <script>
-  module.exports = {
-    created() {
-      this.chartData = {
-        dimensions: {
-          name: 'Week',
-          data: ['Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fir.', 'Sat.', 'Sun.'],
-        },
-        measures: [
-          {
-            name: 'Vue',
-            data: [30, 40, 35, 50, 49, 70, 90],
+  import { onMounted } from 'vue';
+  import * as echarts from 'echarts';
+  export default {
+    setup() {
+      onMounted(() => {
+        const myChart = echarts.init(document.getElementById('leibie'));
+        myChart.setOption({
+          xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
           },
-        ],
-      };
+          yAxis: {
+            type: 'value',
+          },
+          series: [
+            {
+              data: [150, 230, 224, 218, 135, 147, 260],
+              type: 'line',
+            },
+          ],
+        });
+      });
     },
   };
 </script>
+<style>
+  .leibie-pie {
+    height: 25rem;
+    width: 35rem;
+  }
+</style>
