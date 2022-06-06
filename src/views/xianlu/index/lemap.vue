3ead398f10532a2967f9914cf12e03721859a7a2
shandongtlb <wangyang19980320@hotmail.com>
2022-06-07 00:03:47 +0800

    添加坐标

--- src/views/xianlu/index/lemap.vue
+++ src/views/xianlu/index/lemap.vue
@@ -36,6 +36,24 @@
           //detectRetina: true,
         }).addTo(this.map);
 
+        const DefaultIcon = L.icon({
+          iconUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+          iconRetinaUrl: require('leaflet/dist/images/marker-icon.png'),
+          shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+          iconSize: [25, 41],
+          iconAnchor: [12, 41],
+          popupAnchor: [1, -34],
+          tooltipAnchor: [16, -28],
+          shadowSize: [41, 41],
+        });
+        L.Marker.prototype.options.icon = DefaultIcon;
+
+        L.marker([36.79107, 117.86078]).addTo(this.map).bindPopup('周村站').openPopup();
+        L.marker([36.796958, 117.742712]).addTo(this.map).bindPopup('邹平站').openPopup();
+        L.marker([36.767247, 117.917215]).addTo(this.map).bindPopup('周村东站').openPopup();
+        L.marker([36.795783, 118.000257]).addTo(this.map).bindPopup('马尚站').openPopup();
+        L.marker([36.78892, 118.057655]).addTo(this.map).bindPopup('淄博站').openPopup();
+        L.marker([36.895439, 118.026388]).addTo(this.map).bindPopup('淄博北站').openPopup();
         //this.map.removeLayer(name); // 移除图层
       },
     },
